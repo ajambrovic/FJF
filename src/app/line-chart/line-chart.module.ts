2f13ed9819226f6586e5bfc73de909ca9eb04ad8
Andrija Jambrovic <andrija.jambrovic@solversint.com>
2018-05-20 15:02:20 +0200

    chart.js

--- src/app/line-chart/line-chart.module.ts
+++ src/app/line-chart/line-chart.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LineChartService } from './line-chart.service';
 import { LineChartComponent } from './line-chart.component';
 import { ToasterModule } from 'angular2-toaster';
+import 'chart.js/dist/Chart.min.js';
 import { ChartsModule } from 'ng2-charts';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
