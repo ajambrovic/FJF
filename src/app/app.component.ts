80f17316cb461a469ff56c25701aef99d7887474
Andrija Jambrovic <andrija.jambrovic@solversint.com>
2018-05-20 19:40:33 +0200

    Layout cleanuo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './login/services/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Smart Habits';
+
+  constructor(
+    public authenticationService: AuthenticationService) { }
 }
