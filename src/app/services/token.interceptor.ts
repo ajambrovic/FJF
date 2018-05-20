6760f2813a238710306512b8c2815491bca15b6d
eanomia <antonio.mileta@ericsson.com>
2018-05-20 18:04:39 +0200

    Fix token interceptor

--- src/app/services/token.interceptor.ts
+++ src/app/services/token.interceptor.ts
@@ -11,18 +11,25 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-    // tslint:disable-next-line:max-line-length
-    private token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiU1lTVEVNIiwiZXhwIjo0NjgxMzYwODI0LCJzdWIiOiJwb3J0YWwtYmFja2VuZCJ9.tbX-WBmBBiXAZPvw0fv-A4SPtQqRsRXy93TzQPEzA_o';
     constructor() { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-        request = request.clone({
-            setHeaders: {
-                'X-Auth-Token': this.token
-            }
-        });
+        const token = this.getToken();
+        if (!!token) {
+            request = request.clone({
+                setHeaders: {
+                    'X-Auth-Token': token
+                }
+            });
+        }
 
         return next.handle(request);
+    }
+
+    private getToken() {
+        const user = JSON.parse(localStorage.getItem('currentUser'));
+        if (!!user) {
+            return user.token;
+        }
     }
 }
