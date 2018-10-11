121b0134d5cf25a6ac2f84f7d1ed1d23c42c8953
eanjamb <andrija.jambrovic@ericsson.com>
2018-10-11 20:56:29 +0200

    Made extending routing more approachable

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,11 +5,11 @@ import { LineChartComponent } from './line-chart/line-chart.component';
 import { AuthGuard } from './common/guards/auth.guard';
 
 const appRoutes: Routes = [
-    { path: '', component: LineChartComponent, canActivate: [AuthGuard] },
+    { path: 'line-chart', component: LineChartComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
 
     // otherwise redirect to home
-    { path: '**', redirectTo: '' }
+    { path: '**', redirectTo: 'line-chart' }
 ];
 
 export const RoutingModule = RouterModule.forRoot(appRoutes);
