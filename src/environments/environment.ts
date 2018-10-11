9d2e48ca54353f393f837e3581f1d7918671f52d
eanjamb <andrija.jambrovic@ericsson.com>
2018-10-11 20:12:49 +0200

    Updated endpoint configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,10 +14,10 @@ export const environment = {
 @Injectable()
 export class GeneralConfigImpl implements AGeneralConfig {
   production = false;
-  homeId = 'MTFjM2IyYTQtZGRmZi00YTI4LWI3YWEtYmQyMjlmZTUwNGEw';
-  userId = 'NGViYWZkMmQtYzI4YS00YTlmLWExYmQtN2YyMWUyYzRhODM5';
-  backendUrl = 'https://portal.smarthabits.io/portal-backend/';
-  loginEndpoint = 'https://portal.smarthabits.io/login-service/login/caregiver';
+  homeId = 'NGE5OTQzZmMtZmM3ZC00N2U5LWFmODQtM2E4NTdiYzBkZmYy';
+  userId = 'ODA3MjIxOTMtYjJjMy00MmVlLTllODctMDVmNDY1N2MxOWFj';
+  backendUrl = 'https://demo.smarthabits.io/portal-backend/';
+  loginEndpoint = 'https://demo.smarthabits.io/login-service/login/caregiver';
   homeStatusEndpoint = this.backendUrl + `homes/${this.homeId}/aggregatedstatus`;
   chartDataEndpoint = this.backendUrl + `users/${this.userId}/chartdata/value`;
   dateFormat = 'dd.MM.yyyy';
@@ -28,7 +28,7 @@ export class GeneralConfigImpl implements AGeneralConfig {
 @Injectable()
 export class LineChartComponentConfigImpl implements ALineChartConfig {
   lineChartType = 'line';
-  defaultNumberOfDays = 30;
+  defaultNumberOfDays = 180;
   lineChartOptions: any = {
     responsive: true,
     legend: {
