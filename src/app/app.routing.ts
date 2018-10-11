84cfa4b1a08e76c6d5b18a9d2281e3c4f9a49a2b
eanjamb <andrija.jambrovic@ericsson.com>
2018-10-11 21:03:18 +0200

    Added route to module
    Also, set the route as the default one.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,8 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LineChartComponent } from './line-chart/line-chart.component';
 import { AuthGuard } from './common/guards/auth.guard';
+import { DoorSensorComponent } from './door-sensor/door-sensor.component';
 
 const appRoutes: Routes = [
+    { path: 'door-sensor', component: DoorSensorComponent, canActivate: [AuthGuard] },
     { path: 'line-chart', component: LineChartComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
 
