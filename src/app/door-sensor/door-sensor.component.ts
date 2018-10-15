2c16b309b908cd89deab096f2d425b5800f11591
eanjamb <andrija.jambrovic@ericsson.com>
2018-10-15 16:14:42 +0200

    Copied required properties and functions

--- src/app/door-sensor/door-sensor.component.ts
+++ src/app/door-sensor/door-sensor.component.ts
@@ -1,4 +1,8 @@
+import { DoorSensorService } from './door-sensor.service';
+import { Colors } from 'ng2-charts';
 import { Component, OnInit } from '@angular/core';
+import { ALineChartConfig } from '../line-chart/line-chart-config';
+import { ApplicationError } from '../common/domain/application.error';
 
 @Component({
   selector: 'app-door-sensor',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoorSensorComponent implements OnInit {
 
-  constructor() { }
+  public isDataAvailable = false;
+  public lineChartData: Array<Colors>;
 
-  ngOnInit() {
+  // config start
+  public lineChartType = this.lineChartConfig.lineChartType;
+  public numberOfDays = this.lineChartConfig.defaultNumberOfDays;
+  public lineChartOptions = this.lineChartConfig.lineChartOptions;
+  public lineChartColors = this.lineChartColors;
+  public selectValues = this.lineChartConfig.selectDropdownValues;
+  public lineChartLabels = this.lineChartConfig.lineChartLabels;
+  // config end
+
+  constructor(
+    private lineChartConfig: ALineChartConfig,
+    private service: DoorSensorService
+  ) { }
+
+
+  ngOnInit(): void {
+    this.getDataForNumberOfDays(this.numberOfDays);
+  }
+
+  private getDataForNumberOfDays(numberOfDays: number) {
+    this.service.getDoorSensorData(numberOfDays).subscribe(
+      lineChartData => {
+        this.lineChartData = lineChartData;
+        this.isDataAvailable = true;
+      },
+      error => {
+        throw new ApplicationError(error);
+      }
+    );
   }
 
 }
